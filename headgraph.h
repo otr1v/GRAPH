d6dec8718bee57b6ac53e339197ee065ddf35022
otr1v <100768041+otr1v@users.noreply.github.com>
2023-07-19 13:14:43 +0300

    Update headgraph.h

--- headgraph.h
+++ headgraph.h
@@ -1,29 +1,57 @@
 #pragma once
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
 
-const int AMOUNT_OF_NODES = 10;
-struct node
+
+#define CHECK_ERR(condition, message_error)           \
+                do{                                    \
+                        if (condition){                 \
+                            printf("%s", message_error); \
+                        }                                 \
+                } while(0)
+
+
+const int START_NUMNODES   = 8;
+const int POISON           = -2281337;
+const int MAX_COMMAND_SIZE = 15;
+struct PostOrder
 {
-    int value;
+    int* visited;
+    int* base;
+    int counter;
+};
+struct Graph
+{
+    
+    int* vertices;
+    int numnodes;
+    int curnumnodes;
+    bool** edges;
+    int root;
+    struct PostOrder postorder;
 };
 
-struct edge
+struct Test
 {
-    node* src;
-    node* dest;
+    FILE* answer;
+    int* neededresult;
+    int* programresult;
 };
-
-struct graph
-{
-    struct edge head[AMOUNT_OF_NODES];
-};
-
-void AddNode(struct node base[]);
-void AddEdge(struct graph grph);
-int ReadCommands();
-// typedef struct node
-// {
-//     char*        text;
-//     struct node* left;
-//     struct node* right;
-// }Node;
+struct Graph* CreateGraph();
+void AddNode(struct Graph* graph, FILE* base);
+void AddEdge(struct Graph* graph, FILE* base);
+bool HasNode(struct Graph* graph, int* vertice);
+void ReallocGraph(struct Graph* graph);
+void RemoveNode(struct Graph* graph, FILE* base);
+void RemoveEdge(struct Graph* graph, FILE* base);
+void Root(struct Graph* graph, FILE* base);
+void RPO(struct Graph* graph, int curnode);
+void PrintRPO(struct Graph* graph, FILE* answer);
+bool IsVisited(struct Graph* graph, int index);
+void ReadCommands(struct Graph* grph, FILE* base, FILE* answer);
+void PrintGraph(struct Graph* graph);
+void Tests(struct Graph* graph, struct Test* test, FILE* base);
+bool Verifier(struct Graph* graph, struct Test* test, FILE* base);
+void DestroyGraph(struct Graph* graph);
