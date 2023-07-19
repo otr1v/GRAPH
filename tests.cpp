63314857f399dfe8b22cbb32ab63a0402fccdf54
otr1v <100768041+otr1v@users.noreply.github.com>
2023-07-19 13:16:58 +0300

    Create tests.cpp

---
+++ tests.cpp
@@ -0,0 +1,12 @@
+#include "headgraph.h"
+
+int main()
+{
+    struct Graph* graph = {};
+
+    struct Test* test = (struct Test*) malloc(sizeof(struct Test));
+    FILE* base = {};
+    Tests(graph, test, base);
+    
+    return 0;
+}
