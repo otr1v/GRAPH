5d5d33c0c8fcb7ce8d3d6395ac50977b65cea762
otr1v <100768041+otr1v@users.noreply.github.com>
2023-07-19 20:07:02 +0300

    Update graph.cpp

--- graph.cpp
+++ graph.cpp
@@ -1,10 +1,8 @@
 #include "headgraph.h"
-
 int main()
 {
     struct Graph* graph = CreateGraph();
-    
-    ReadCommands(graph, stdin, stdout);
+    ReadCommands(graph, stdin, stdout, 0);
     DestroyGraph(graph);
 
     return 0;
